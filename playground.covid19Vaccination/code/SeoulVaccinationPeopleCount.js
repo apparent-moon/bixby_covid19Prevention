4e64a398da8b1a7b12471dfb4b2350764adb5ab7
apmoon <ohappy0206@naver.com>
2021-06-04 01:19:14 +0900

    apmoon ) JS code edited & date layout added
    js 코드수정(d-day 계산하는방식으로 변경) & date 관련하여 layout 추가 및 dialog 수정

--- playground.covid19Vaccination/code/SeoulVaccinationPeopleCount.js
+++ playground.covid19Vaccination/code/SeoulVaccinationPeopleCount.js
@@ -18,40 +18,45 @@ module.exports.function = function SeoulVaccinationPeopleCount(date) {
     //date가 존재할 경우
 
     var today = now.getDateTime();
-    var dateZ = dates.ZonedDateTime.fromDate(date) //date를 ZonedDateTime 형식으로 바꾼것
+    //var dateZ = dates.ZonedDateTime.fromDate(date) //date를 ZonedDateTime 형식으로 바꾼것
+    
+      //오늘 날짜의 값
+      var todayYear = today.date.year
+      //month에 -1을 해야 new Date할 때 month가 제대로 나옴.
+      var todayMonth = today.date.month - 1
+      var todayDay = today.date.day
 
-    if (first_Data_Date.isBefore(dateZ) && !first_Data_Date.isAfter(dateZ)) {
+      //사용자가 발화한 날짜의 값
+      var dateYear = date.year
+      var dateMonth = date.month - 1
+      var dateDay = date.day
 
-      console.log("1")
-      // 2021년 4월 21일 이후 부터 오늘까지
+      var Today = new Date(todayYear, todayMonth, todayDay, 0, 0, 0, 0)
+      var Dday = new Date(dateYear, dateMonth, dateDay, 0, 0, 0, 0)
+      var FirstDate = new Date(2021, 3, 21,0,0,0,0)
 
-      if (today.date.year == date.year && today.date.month == date.month && today.date.day == date.day) {
-        // 오늘의 결과는 보여줄 수 없으므로 어제 결과를 보여줌.
-        var start_Index = 1;
-        var end_Index = 1;
-      } else {
-        var periodHour = now.durationUntil(dateZ).periodHours
-        var periodDay = now.durationUntil(dateZ).periodDays
-        if (periodDay == 1 || periodDay == undefined && periodHour == 24) {
-          //어제. 값이 계속 변동되어서 2가지 조건 주었음
-          var start_Index = 1;
-        } else {
-          start_Index = periodDay
-        }
-        end_Index = start_Index;
-      }
+      console.log(Dday);
+      console.log(FirstDate);
 
-    } else {
-      //2021년 4월 21일 이전 혹은 오늘 이후의 값을 요구할경우 어제의 값을 보여준다.
+      var gap = Dday.getTime() - Today.getTime()
+      var dday_Result = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1
+
+      var firstDataGap = FirstDate.getTime() - Today.getTime()
+      var firstDataGap_Result = Math.floor(firstDataGap / (1000 * 60 * 60 * 24)) * -1
+
+      //dday 결과값이 오늘이 아니고, 미래의 값이 아닐 경우에
+      if(dday_Result !=  0 && dday_Result > 0 && dday_Result <= firstDataGap_Result){
+      var start_Index = dday_Result
+      var end_Index = start_Index
+      }else{
       var start_Index = 1;
       var end_Index = 1;
-    }
+      }
   } else {
     //date값이 들어오지 않을때는 default값으로 어제 결과를 보여줌.
     var start_Index = 1;
     var end_Index = 1;
   }
-
 
   var url = baseurl + key + type + service + start_Index + "/" + end_Index + "/"
 
@@ -59,11 +64,22 @@ module.exports.function = function SeoulVaccinationPeopleCount(date) {
 
   target = response.tvCorona19VaccinestatNew.row[0].FIR_SUB.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") //숫자에 ,를 넣는것. 이렇게 해서 넣으려면 primitive 타입을 text로 변경 해줘야 합니다. 현재는 integer라서 들어가지않음!
 
+  dateResponseDate = response.tvCorona19VaccinestatNew.row[0].S_VC_DT.split('.')
+  
+  responseYear = parseInt(dateResponseDate[0])
+  responseMonth = parseInt(dateResponseDate[1])
+  responseDay = parseInt(dateResponseDate[2])
+
   return {
     seoulInoculationTarget: response.tvCorona19VaccinestatNew.row[0].FIR_SUB,
     peopleCount_1st: response.tvCorona19VaccinestatNew.row[0].FIR_INC1,
     peopleCount_1st_Sum: response.tvCorona19VaccinestatNew.row[0].FIR_INC,
     peopleCount_2nd: response.tvCorona19VaccinestatNew.row[0].SCD_INC1,
-    peopleCount_2nd_Sum: response.tvCorona19VaccinestatNew.row[0].SCD_INC
+    peopleCount_2nd_Sum: response.tvCorona19VaccinestatNew.row[0].SCD_INC,
+    vaccinationDate: {
+      vaccinationYear : responseYear,
+      vaccinationMonth : responseMonth,
+      vaccinationDay : responseDay
+    }
   }
 }
