e0e8c8997fecee365182d632af3344a55dda8af9
apmoon <ohappy0206@naver.com>
2021-06-08 00:51:04 +0900

    apmoon ) JS code edited & FalseDate primitive added - 20210608 00:50
    JS code 수정 및  FalseDate primitive 추가 for dialog 사용

--- playground.covid19Vaccination/code/SeoulVaccinationPeopleCount.js
+++ playground.covid19Vaccination/code/SeoulVaccinationPeopleCount.js
@@ -8,15 +8,17 @@ const key = "66715342496f6861393448414a4c49/"
 const type = "json/"
 const service = "tvCorona19VaccinestatNew/"
 var start_Index, end_Index;
-var apiupdate = true
 
 module.exports.function = function SeoulVaccinationPeopleCount(date) {
+
+  let apiupdate = true;
+  let falseDate = false;
 
   const now = dates.ZonedDateTime.now()
   const first_Data_Date = dates.ZonedDateTime.of(2021, 4, 21); //이날부터 데이터가 있습니다. 그전 데이터는 존재하지 않습니다.
 
   var today = now.getDateTime();
-  //var dateZ = dates.ZonedDateTime.fromDate(date) //date를 ZonedDateTime 형식으로FIR_SUB 바꾼것
+  //var dateZ = dates.ZonedDateTime.fromDate(date) //date를 ZonedDateTime 형식으로 바꾼것
 
   //오늘 날짜의 값
   var todayYear = today.date.year
@@ -29,46 +31,15 @@ module.exports.function = function SeoulVaccinationPeopleCount(date) {
   var newToday = new Date(todayYear, todayMonth, todayDay, 0, 0, 0, 0)
   var yesterDay = new Date(todayYear, todayMonth, yesterdayDay, 0, 0, 0, 0)
 
-  //date가 존재할 경우
-  if (date) {
 
-    //사용자가 발화한 날짜의 값
-    var dateYear = date.year
-    var dateMonth = date.month - 1
-    var dateDay = date.day
-
-    var dday = new Date(dateYear, dateMonth, dateDay, 0, 0, 0, 0)
-    console.log("dday",dday)
-    var firstDate = new Date(2021, 3, 21, 0, 0, 0, 0)
-
-    var gap = dday.getTime() - newToday.getTime()
-    var dday_Result = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1
-
-    var firstDataGap = firstDate.getTime() - newToday.getTime()
-    var firstDataGap_Result = Math.floor(firstDataGap / (1000 * 60 * 60 * 24)) * -1
-
-    //dday 결과값이 오늘이 아니고, 미래의 값이 아닐 경우에 (= 정상적인 값일 때)
-    if (dday_Result != 0 && dday_Result > 0 && dday_Result <= firstDataGap_Result) {
-      var start_Index = dday_Result - 1 //apiupdate = false를 대응하기 위해 +1
-      var end_Index = start_Index + 1
-    } else {
-      var start_Index = 1;
-      var end_Index = 2;
-    }
-  } else {
-    //date값이 들어오지 않을때는 default값으로 어제 결과를 보여줌.
-    var start_Index = 1;
-    var end_Index = 2;
-  }
+  // apiUpdate 여부 확인을 위한 api호출
+  var start_Index = 1;
+  var end_Index = 1;
 
   var url = baseurl + key + type + service + start_Index + "/" + end_Index + "/"
+  const updateCheck_Response = http.getUrl(url, { format: 'json' });
 
-  // info return from api
-  const response = http.getUrl(url, { format: 'json' });
-
-  //만약 date에서 요구한값이 4월 21일 이전 값이거나 미래의 값이고 또는 내일의 값을 요구했는데 0번째 값이 어제의 값이 아니라면 apiupdate false라고 한다.
-
-  dateResponseDate = response.tvCorona19VaccinestatNew.row[0].S_VC_DT.split('.')
+  dateResponseDate = updateCheck_Response.tvCorona19VaccinestatNew.row[0].S_VC_DT.split('.')
 
   responseYear = parseInt(dateResponseDate[0])
   responseMonth = parseInt(dateResponseDate[1])
@@ -77,57 +48,102 @@ module.exports.function = function SeoulVaccinationPeopleCount(date) {
   //API에서 넘겨주는 값
   responseDate = new Date(responseYear, responseMonth - 1, responseDay, 0, 0, 0, 0)
 
-  if (yesterDay != responseDate) {
+  if (Number(yesterDay) != Number(responseDate)) {
     var apiupdate = false
   }
+  console.log("apiupdate : " , apiupdate)
 
-  if (apiupdate) { //true면
+  if (apiupdate) { // api = true
+    if (date) {
+      //사용자가 발화한 날짜의 값
+      var dateYear = date.year
+      var dateMonth = date.month - 1
+      var dateDay = date.day
 
-    console.log("apiupdate : true")
+      var dday = new Date(dateYear, dateMonth, dateDay, 0, 0, 0, 0)
+      console.log("dday", dday)
+      var firstDate = new Date(2021, 3, 21, 0, 0, 0, 0)
 
-    target = response.tvCorona19VaccinestatNew.row[1].FIR_SUB.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") //숫자에 ,를 넣는것. 이렇게 해서 넣으려면 primitive 타입을 text로 변경 해줘야 합니다. 현재는 integer라서 들어가지않음!
+      var gap = dday.getTime() - newToday.getTime()
+      var dday_Result = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1
 
-    dateResponseDate = response.tvCorona19VaccinestatNew.row[1].S_VC_DT.split('.')
+      var firstDataGap = firstDate.getTime() - newToday.getTime()
+      var firstDataGap_Result = Math.floor(firstDataGap / (1000 * 60 * 60 * 24)) * -1
 
-    responseYear = parseInt(dateResponseDate[0])
-    responseMonth = parseInt(dateResponseDate[1])
-    responseDay = parseInt(dateResponseDate[2])
-
-    return {
-      seoulInoculationTarget: response.tvCorona19VaccinestatNew.row[1].FIR_SUB,
-      peopleCount_1st: response.tvCorona19VaccinestatNew.row[1].FIR_INC1,
-      peopleCount_1st_Sum: response.tvCorona19VaccinestatNew.row[1].FIR_INC,
-      peopleCount_2nd: response.tvCorona19VaccinestatNew.row[1].SCD_INC1,
-      peopleCount_2nd_Sum: response.tvCorona19VaccinestatNew.row[1].SCD_INC,
-      vaccinationDate: {
-        vaccinationYear: responseYear,
-        vaccinationMonth: responseMonth,
-        vaccinationDay: responseDay
-      },
-      apiUpdate: apiupdate
+      //dday 결과값이 오늘이 아니고, 미래의 값이 아닐 경우에 (= 정상적인 값일 때)
+      if (dday_Result != 0 && dday_Result > 0 && dday_Result <= firstDataGap_Result) {
+        var start_Index = dday_Result
+        var end_Index = start_Index
+      } else {
+        var falseDate = true;
+        var start_Index = 1;
+        var end_Index = 1;
+      }
+    } else {
+      //date값이 들어오지 않을때는 default값으로 어제 결과를 보여줌.
+      var start_Index = 1;
+      var end_Index = 1;
     }
-  } else {
-    console.log("apiupdate : false")
+  } else { //api =false
+    if (date) {
+      //사용자가 발화한 날짜의 값
+      var dateYear = date.year
+      var dateMonth = date.month - 1
+      var dateDay = date.day
 
-    target = response.tvCorona19VaccinestatNew.row[0].FIR_SUB.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") //숫자에 ,를 넣는것. 이렇게 해서 넣으려면 primitive 타입을 text로 변경 해줘야 합니다. 현재는 integer라서 들어가지않음!
+      var dday = new Date(dateYear, dateMonth, dateDay, 0, 0, 0, 0)
+      console.log("dday", dday)
+      var firstDate = new Date(2021, 3, 21, 0, 0, 0, 0)
 
-    dateResponseDate = response.tvCorona19VaccinestatNew.row[0].S_VC_DT.split('.')
+      var gap = dday.getTime() - newToday.getTime()
+      var dday_Result = Math.floor(gap / (1000 * 60 * 60 * 24)) * -1
 
-    responseYear = parseInt(dateResponseDate[0])
-    responseMonth = parseInt(dateResponseDate[1])
-    responseDay = parseInt(dateResponseDate[2])
-    return {
-      seoulInoculationTarget: response.tvCorona19VaccinestatNew.row[0].FIR_SUB,
-      peopleCount_1st: response.tvCorona19VaccinestatNew.row[0].FIR_INC1,
-      peopleCount_1st_Sum: response.tvCorona19VaccinestatNew.row[0].FIR_INC,
-      peopleCount_2nd: response.tvCorona19VaccinestatNew.row[0].SCD_INC1,
-      peopleCount_2nd_Sum: response.tvCorona19VaccinestatNew.row[0].SCD_INC,
-      vaccinationDate: {
-        vaccinationYear: responseYear,
-        vaccinationMonth: responseMonth,
-        vaccinationDay: responseDay
-      },
-      apiUpdate: apiupdate
+      var firstDataGap = firstDate.getTime() - newToday.getTime()
+      var firstDataGap_Result = Math.floor(firstDataGap / (1000 * 60 * 60 * 24)) * -1
+
+      console.log(dday_Result, firstDataGap_Result)
+
+      //dday 결과값이 어오늘이 아니고, 미래의 값이 아닐 경우에 (= 정상적인 값일 때) , 어제 값을 요구하지 않을 때
+      if (dday_Result != 0 && dday_Result > 0 && dday_Result <= firstDataGap_Result && dday_Result != 1) {
+        var start_Index = dday_Result - 1 //apiupdate = false를 대응하기 위해 +1
+        var end_Index = start_Index
+      } else {
+        var falseDate = true;
+        var start_Index = 1;
+        var end_Index = 1;
+      }
+    } else {
+      //date값이 들어오지 않을때는 default값으로 최신 결과를 보여줌.
+      var start_Index = 1;
+      var end_Index = 1;
     }
+  }
+
+  var url = baseurl + key + type + service + start_Index + "/" + end_Index + "/"
+
+  // info return from api
+  const response = http.getUrl(url, { format: 'json' });
+
+  target = response.tvCorona19VaccinestatNew.row[0].FIR_SUB.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") //숫자에 ,를 넣는것. 이렇게 해서 넣으려면 primitive 타입을 text로 변경 해줘야 합니다. 현재는 integer라서 들어가지않음!
+
+  dateResponseDate = response.tvCorona19VaccinestatNew.row[0].S_VC_DT.split('.')
+
+  responseYear = parseInt(dateResponseDate[0])
+  responseMonth = parseInt(dateResponseDate[1])
+  responseDay = parseInt(dateResponseDate[2])
+
+  return {
+    seoulInoculationTarget: response.tvCorona19VaccinestatNew.row[0].FIR_SUB,
+    peopleCount_1st: response.tvCorona19VaccinestatNew.row[0].FIR_INC1,
+    peopleCount_1st_Sum: response.tvCorona19VaccinestatNew.row[0].FIR_INC,
+    peopleCount_2nd: response.tvCorona19VaccinestatNew.row[0].SCD_INC1,
+    peopleCount_2nd_Sum: response.tvCorona19VaccinestatNew.row[0].SCD_INC,
+    vaccinationDate: {
+      vaccinationYear: responseYear,
+      vaccinationMonth: responseMonth,
+      vaccinationDay: responseDay
+    },
+    apiUpdate: apiupdate,
+    falseDate : falseDate
   }
 }
